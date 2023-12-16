d7844853f571f884c45b2e2c4d531118f9ef3f5b
usiqwerty <benlogin1@protonmail.com>
2023-12-16 18:04:19 +0500

    Moved to sqlite database, improved task key generation

--- cached_requests.py
+++ cached_requests.py
@@ -1,58 +1,56 @@
+import sqlite3
 import json
-
 import requests
 
 fn_cache = "cache.json"
 fn_stat = "stat.json"
 cache = {}
 stat = {}
+connection: sqlite3.Connection
+cursor: sqlite3.Cursor
 
+fn_db = 'my_database.db'
+def gen_key(host, quizId, questionId):
+	host_acr = ''.join(map(lambda x: x[0], host.split('.')))
+	return f"{host_acr}-{quizId}-{questionId}"
 
 def load():
-	global cache, stat
-	try:
-		with open(fn_cache) as f:
-			print(f"Reading {fn_cache}")
-			cache = json.load(f)
-			print(len(cache.keys()), "entries read")
-	except FileNotFoundError:
-		cache = {}
-	try:
-		with open(fn_stat) as f:
-			print(f"Reading {fn_stat}")
-			stat = json.load(f)
-	except FileNotFoundError:
-		stat = {"ss-req-count": 0, "rs-req-count": 0}
-	print(stat)
+	global connection, cursor
+	connection = sqlite3.connect(fn_db, check_same_thread=False)
+	cursor = connection.cursor()
 
 
 def save():
-	with open(fn_cache, "w") as f:
-		print(f"Saving to {fn_cache}")
-		json.dump(cache, f)
-		print("Done")
-	with open(fn_stat, "w") as f:
-		print(f"Saving to {fn_stat}")
-		json.dump(stat, f)
-		print("Done")
-	return f"Saved {len(cache.keys())} entries"
+	connection.close()
+#[{'anchor': ['А. И. Покрышкин'], 'suggestions': [{'correctness': 2, 'confidence': 0.99, 'label': 'true', 'data': {'checked': True}}], 'submissions': [{'correctness': 2, 'count': 3800, 'label': 'true', 'data': {'checked': True}}, {'correctness': 0, 'count': 207, 'label': 'false', 'data': {'checked': False}}]}, {'anchor': ['G. K. Zhukov'], 'suggestions': [{'correctness': 2, 'confidence': 0.99, 'label': 'true', 'data': {'checked': True}}], 'submissions': [{'correctness': 2, 'count': 1, 'label': 'true', 'data': {'checked': True}}]}, {'anchor': ['Н. Ф. Гастелло'], 'suggestions': [{'correctness': 2, 'confidence': 0.99, 'label': 'false', 'data': {'checked': False}}, {'correctness': 2, 'confidence': 0.99, 'label': 'true', 'data': {'checked': True}}], 'submissions': [{'correctness': 2, 'count': 3816, 'label': 'false', 'data': {'checked': False}}, {'correctness': 0, 'count': 84, 'label': 'true', 'data': {'checked': True}}]}, {'anchor': ['И. Н. Кожедуб'], 'suggestions': [{'correctness': 2, 'confidence': 0.99, 'label': 'true', 'data': {'checked': True}}], 'submissions': [{'correctness': 2, 'count': 3774, 'label': 'true', 'data': {'checked': True}}, {'correctness': 0, 'count': 231, 'label': 'false', 'data': {'checked': False}}]}, {'anchor': ['Г. К. Жуков'], 'suggestions': [{'correctness': 2, 'confidence': 0.99, 'label': 'true', 'data': {'checked': True}}], 'submissions': [{'correctness': 2, 'count': 3781, 'label': 'true', 'data': {'checked': True}}, {'correctness': 0, 'count': 223, 'label': 'false', 'data': {'checked': False}}]}, {'anchor': ['N. F. Gastello'], 'suggestions': [{'correctness': 2, 'confidence': 0.99, 'label': 'false', 'data': {'checked': False}}], 'submissions': [{'correctness': -1, 'count': 1, 'label': 'false', 'data': {'checked': False}}]}, {'anchor': ['I. N. Kozhedub'], 'suggestions': [{'correctness': 2, 'confidence': 0.99, 'label': 'true', 'data': {'checked': True}}], 'submissions': [{'correctness': 2, 'count': 1, 'label': 'true', 'data': {'checked': True}}]}, {'anchor': ['A. I. Pokryshkin'], 'suggestions': [{'correctness': 2, 'confidence': 0.99, 'label': 'true', 'data': {'checked': True}}], 'submissions': [{'correctness': 2, 'count': 1, 'label': 'true', 'data': {'checked': True}}]}]
 
 
 def save_to_cache(key, ss_solution):
-	global cache
-	cache[key] = ss_solution
-
+	ss_solution=ss_solution.replace("\'", "\"")
+	ss_solution=ss_solution.replace("True", "true")
+	ss_solution = ss_solution.replace("False", "false")
+	req=f"INSERT INTO tasks VALUES ('{key}', '{ss_solution}');"
+	print("req:", req)
+	cursor.execute(req)
+	cursor.execute("COMMIT;");
 
 def get_from_cache(key):
-	return cache[key]
+	a = cursor.execute(f"SELECT solution FROM tasks WHERE task_key = '{key}';")
+	for r in a:
+		return str(r[0])
 
 
 def present_in_cache(key):
-	return key in cache
-def stat_add(ss:int, rs:int):
-	global stat
-	stat["ss-req-count"] += ss
-	stat["rs-req-count"] += rs
+	a = get_from_cache(key)
+	return bool(a)
+
+
+def stat_add(ss: int, rs: int):
+	# global stat
+	# stat["ss-req-count"] += ss
+	# stat["rs-req-count"] += rs
+	pass
+
 
 def cached_get(args):
 	global cache
@@ -65,7 +63,7 @@ def cached_get(args):
 	client = "1.1.6"
 	host = args["host"]  # "elearn.urfu.ru"
 
-	if questionType=="unspecified":
+	if questionType == "unspecified":
 		return "Unspecified question type", 400
 
 	api_base_url = "https://syncshare.naloaty.me/api/v2"
@@ -79,13 +77,28 @@ def cached_get(args):
 		   f"{questionType=!s}&"
 		   f"{client=!s}")
 
-	key = f"{quizId}-{questionId}"
+	key = gen_key(host, quizId, questionId)
 	if not present_in_cache(key):
 		print(url)
-		stat_add(1,0)
-		ss_solution = requests.get(url).json()
+		stat_add(1, 0)
+		ss_solution = str(requests.get(url).json())
+		print(ss_solution)
 		save_to_cache(key, ss_solution)
 
 	else:
 		stat_add(0, 1)
 	return get_from_cache(key)
+
+
+if __name__ == "__main__":
+	conn = sqlite3.connect(fn_db)
+	cur = conn.cursor()
+	for r in cur.execute("select * from tasks;"): print(r)
+	cur.execute("insert into tasks values ('a', 'b');")
+	cur.execute("commit;")
+	#cmd = input("SQL: ")
+	#while cmd != "exit":
+	#	ans = cur.execute(cmd)
+	#	for row in ans: print(row)
+	#	cmd = input("SQL: ")
+	conn.close()
--- syncshare_api.py
+++ syncshare_api.py
@@ -1,14 +1,16 @@
-from cached_requests import cached_get
-
+from cached_requests import cached_get, load
+from json import loads
 
 def get_syncshare_solution(args):
-	r_json = cached_get(args)
-	return r_json
+	r = cached_get(args)
+	print(f"{r=}")
+	return loads(r)
 
 
 if __name__ == "__main__":
 	aaa = [('host', 'elearn.urfu.ru'), ('courseId', '7072'), ('quizId', '200118'), ('attemptId', '968122'),
 		   ('moodleId', '75021'), ('questionId', '3025539'), ('questionType', 'multichoice')]
+	load()
 	ar = {}
 	for k, v in aaa:
 		ar[k] = v
